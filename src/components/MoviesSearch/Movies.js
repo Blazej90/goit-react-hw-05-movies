e016e8f2a7c7f3a7c1c90f50dbf511201b79b574
Błażej Bartoszewski <blazejbart@gmail.com>
2024-02-15 10:42:29 +0100

    add pressKey enter

--- src/components/MoviesSearch/Movies.js
+++ src/components/MoviesSearch/Movies.js
@@ -1,3 +1,70 @@
+// import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
+// import axios from 'axios';
+// import PropTypes from 'prop-types';
+// import styles from './Movies.module.css';
+
+// const Movies = () => {
+//   const [query, setQuery] = useState('');
+//   const [searchResults, setSearchResults] = useState([]);
+
+//   const handleSearch = async () => {
+//     try {
+//       const response = await axios.get(
+//         `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=a067f81bd7a94c3876fea33a53d4c87a`
+//       );
+//       setSearchResults(response.data.results);
+//     } catch (error) {
+//       console.error('Error searching movies:', error);
+//     }
+//   };
+
+//   return (
+//     <div className={styles.containerSearch}>
+//       <h1>Search Movies</h1>
+//       <input
+//         type="text"
+//         value={query}
+//         onChange={e => setQuery(e.target.value)}
+//         className={styles.labelSearch}
+//       />
+//       <button onClick={handleSearch} className={styles.btnSearch}>
+//         {' '}
+//         Search
+//       </button>
+//       <ul className={styles.containerPosters}>
+//         {searchResults.map(movie => (
+//           <li key={movie.id} className={styles.posterItem}>
+//             <Link to={`/movies/${movie.id}`} className={styles.posterLink}>
+//               {movie.poster_path && (
+//                 <img
+//                   className={styles.posterImage}
+//                   src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+//                   alt={movie.title}
+//                 />
+//               )}
+//               <span className={styles.posterTitle}>{movie.title}</span>
+//             </Link>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// Movies.propTypes = {
+//   query: PropTypes.string.isRequired,
+//   searchResults: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.number.isRequired,
+//       title: PropTypes.string.isRequired,
+//       poster_path: PropTypes.string,
+//     })
+//   ).isRequired,
+// };
+
+// export default Movies;
+
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
@@ -19,29 +86,37 @@ const Movies = () => {
     }
   };
 
+  const handleKeyPress = event => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
   return (
     <div className={styles.containerSearch}>
-      <h1>Search Movies</h1>
+      <h1 className={styles.searchTitle}>Search Movies</h1>
       <input
         type="text"
         value={query}
         onChange={e => setQuery(e.target.value)}
+        onKeyPress={handleKeyPress}
         className={styles.labelSearch}
       />
       <button onClick={handleSearch} className={styles.btnSearch}>
-        {' '}
         Search
       </button>
       <ul className={styles.containerPosters}>
         {searchResults.map(movie => (
           <li key={movie.id} className={styles.posterItem}>
             <Link to={`/movies/${movie.id}`} className={styles.posterLink}>
-              {movie.poster_path && (
+              {movie.poster_path ? (
                 <img
                   className={styles.posterImage}
                   src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                   alt={movie.title}
                 />
+              ) : (
+                <span className={styles.noPoster}>No poster</span>
               )}
               <span className={styles.posterTitle}>{movie.title}</span>
             </Link>
