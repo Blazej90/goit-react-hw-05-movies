aaef50578636aef348c120654b5ecb9d66753fa5
Błażej Bartoszewski <blazejbart@gmail.com>
2024-02-16 14:33:13 +0100

    change index.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,57 +1,14 @@
-// import React, { Suspense } from 'react';
-// import { Routes, Route, NavLink } from 'react-router-dom';
-// import PropTypes from 'prop-types';
-
-// import styles from './App.module.css';
-// import Home from './HomePage/Home';
-// import Movies from './MoviesSearch/Movies';
-// import MovieDetails from './MovieDetails/MovieDetails';
-// import Cast from './Cast/Cast';
-// import Reviews from './MovieReviews/Reviews';
-
-// const App = ({ children }) => {
-//   return (
-//     <div>
-//       <header>
-//         <NavLink exact="true" to="/" className={styles.activeLink}>
-//           Home
-//         </NavLink>
-//         <NavLink exact="true" to="/movies" className={styles.activeLink}>
-//           Movies
-//         </NavLink>
-//       </header>
-//       <Suspense fallback={<div>Loading...</div>}>
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/movies" element={<Movies />} />
-//           <Route path="/movies/:movieId" element={<MovieDetails />}>
-//             <Route path="/movies/:movieId/cast" element={<Cast />} />
-//             <Route path="reviews" element={<Reviews />} />
-//           </Route>
-//         </Routes>
-//       </Suspense>
-//       {children}
-//     </div>
-//   );
-// };
-
-// App.propTypes = {
-//   children: PropTypes.node,
-// };
-
-// export default App;
-
-// App.jsx
 import React, { Suspense } from 'react';
 import { Routes, Route, NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import styles from './App.module.css';
-import Home from './HomePage/Home';
-import Movies from './MoviesSearch/Movies';
-import MovieDetails from './MovieDetails/MovieDetails';
-import Cast from './Cast/Cast';
-import Reviews from './MovieReviews/Reviews';
+
+const Home = React.lazy(() => import('./HomePage/Home'));
+const Movies = React.lazy(() => import('./MoviesSearch/Movies'));
+const MovieDetails = React.lazy(() => import('./MovieDetails/MovieDetails'));
+const Cast = React.lazy(() => import('./Cast/Cast'));
+const Reviews = React.lazy(() => import('./MovieReviews/Reviews'));
 
 const App = ({ children }) => {
   return (
--- src/components/MoviesSearch/Movies.js
+++ src/components/MoviesSearch/Movies.js
@@ -1,90 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import { Link, useSearchParams } from 'react-router-dom';
-// import axios from 'axios';
-// import PropTypes from 'prop-types';
-// import styles from './Movies.module.css';
-
-// const Movies = () => {
-//   const [localSearchResults, setLocalSearchResults] = useState([]);
-//   const [searchQuery, setSearchQuery] = useState('');
-//   const [searchParams, setSearchParams] = useSearchParams();
-
-//   useEffect(() => {
-//     const queryParam = searchParams.get('query');
-//     setSearchQuery(queryParam || '');
-//     fetchMovies(queryParam);
-//   }, [searchParams]);
-
-//   const fetchMovies = async query => {
-//     try {
-//       const response = await axios.get(
-//         `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=a067f81bd7a94c3876fea33a53d4c87a`
-//       );
-//       setLocalSearchResults(response.data.results);
-//     } catch (error) {
-//       console.error('Error searching movies:', error);
-//     }
-//   };
-
-//   const handleSearch = async () => {
-//     setSearchParams({ query: searchQuery });
-//     fetchMovies(searchQuery);
-//   };
-
-//   const handleKeyPress = event => {
-//     if (event.key === 'Enter') {
-//       handleSearch();
-//     }
-//   };
-
-//   return (
-//     <div className={styles.containerSearch}>
-//       <h1 className={styles.searchTitle}>Search Movies</h1>
-//       <input
-//         type="text"
-//         value={searchQuery}
-//         onChange={e => setSearchQuery(e.target.value)}
-//         onKeyPress={handleKeyPress}
-//         className={styles.labelSearch}
-//       />
-//       <button onClick={handleSearch} className={styles.btnSearch}>
-//         Search
-//       </button>
-//       <ul className={styles.containerPosters}>
-//         {localSearchResults.map(movie => (
-//           <li key={movie.id} className={styles.posterItem}>
-//             <Link to={`/movies/${movie.id}`} className={styles.posterLink}>
-//               {movie.poster_path ? (
-//                 <img
-//                   className={styles.posterImage}
-//                   src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-//                   alt={movie.title}
-//                 />
-//               ) : (
-//                 <span className={styles.noPoster}>No poster</span>
-//               )}
-//               <span className={styles.posterTitle}>{movie.title}</span>
-//             </Link>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// Movies.propTypes = {
-//   query: PropTypes.string,
-//   searchResults: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       id: PropTypes.number.isRequired,
-//       title: PropTypes.string.isRequired,
-//       poster_path: PropTypes.string,
-//     })
-//   ),
-// };
-
-// export default Movies;
-
 import React, { useEffect, useState } from 'react';
 import { Link, useSearchParams } from 'react-router-dom';
 import axios from 'axios';
@@ -102,7 +15,7 @@ const Movies = () => {
     if (queryParam) {
       fetchMovies(queryParam);
     } else {
-      setLocalSearchResults([]); // Clear search results if queryParam is null
+      setLocalSearchResults([]);
     }
   }, [searchParams]);
 
@@ -119,7 +32,6 @@ const Movies = () => {
 
   const handleSearch = async () => {
     if (searchQuery) {
-      // Check if searchQuery is not empty or null
       setSearchParams({ query: searchQuery });
       fetchMovies(searchQuery);
     }
