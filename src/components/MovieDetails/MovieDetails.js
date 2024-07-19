00e213853283ed7dfecf05266484b0282b2cdd1c
Błażej Bartoszewski <blazejbart@gmail.com>
2024-07-19 18:01:28 +0200

    close Cast and Reviews

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,72 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import { Link, Outlet, useParams } from 'react-router-dom';
-// import axios from 'axios';
-// import styles from './MovieDetails.module.css';
-
-// const MovieDetails = () => {
-//   const { movieId } = useParams();
-//   const [movieDetails, setMovieDetails] = useState(null);
-
-//   useEffect(() => {
-//     const fetchMovieDetails = async () => {
-//       try {
-//         const response = await axios.get(
-//           `https://api.themoviedb.org/3/movie/${movieId}?api_key=a067f81bd7a94c3876fea33a53d4c87a&append_to_response=keywords`
-//         );
-//         setMovieDetails(response.data);
-//       } catch (error) {
-//         console.error('Error fetching movie details:', error);
-//       }
-//     };
-
-//     fetchMovieDetails();
-//   }, [movieId]);
-
-//   if (!movieDetails) return <div>Loading...</div>;
-
-//   const handleGoBack = () => {
-//     window.history.back();
-//   };
-
-//   return (
-//     <div className={styles.containerMovieDetails}>
-//       <button onClick={handleGoBack} className={styles.btnGoBack}>
-//         Go back
-//       </button>
-//       <h1>{movieDetails.title}</h1>
-//       <div className={styles.containerPoster}>
-//         {movieDetails.poster_path && (
-//           <img
-//             className={styles.posterImage}
-//             src={`https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`}
-//             alt={movieDetails.title}
-//           />
-//         )}
-//         <p className={styles.description}>{movieDetails.overview}</p>
-//       </div>
-//       <div>
-//         <div className={styles.containerInformation}>
-//           <h3>Additional information</h3>
-//           <div className={styles.castAndReviews}>
-//             <Link to={`/movies/${movieId}/cast`} className={styles.linkCast}>
-//               Cast
-//             </Link>
-//             <Link
-//               to={`/movies/${movieId}/reviews`}
-//               className={styles.linkReviews}
-//             >
-//               Reviews
-//             </Link>
-//           </div>
-//         </div>
-//       </div>
-//       <Outlet />
-//     </div>
-//   );
-// };
-
-// export default MovieDetails;
-
 import React, { useEffect, useState } from 'react';
 import { Link, Outlet, useParams } from 'react-router-dom';
 import axios from 'axios';
@@ -75,6 +6,7 @@ import styles from './MovieDetails.module.css';
 const MovieDetails = () => {
   const { movieId } = useParams();
   const [movieDetails, setMovieDetails] = useState(null);
+  const [activeSection, setActiveSection] = useState(null);
 
   useEffect(() => {
     const fetchMovieDetails = async () => {
@@ -97,6 +29,10 @@ const MovieDetails = () => {
     window.history.back();
   };
 
+  const handleLinkClick = section => {
+    setActiveSection(prev => (prev === section ? null : section));
+  };
+
   return (
     <div className={styles.containerMovieDetails}>
       <button onClick={handleGoBack} className={styles.btnGoBack}>
@@ -113,23 +49,31 @@ const MovieDetails = () => {
         )}
         <p className={styles.description}>{movieDetails.overview}</p>
       </div>
-      <div>
-        <div className={styles.containerInformation}>
-          <h3>Additional information</h3>
-          <div className={styles.castAndReviews}>
-            <Link to={`/movies/${movieId}/cast`} className={styles.linkCast}>
-              Cast
-            </Link>
-            <Link
-              to={`/movies/${movieId}/reviews`}
-              className={styles.linkReviews}
-            >
-              Reviews
-            </Link>
-          </div>
+      <div className={styles.containerInformation}>
+        <h3>Additional information</h3>
+        <div className={styles.castAndReviews}>
+          <Link
+            to={`/movies/${movieId}/cast`}
+            className={`${styles.linkCast} ${
+              activeSection === 'cast' ? styles.activeLink : ''
+            }`}
+            onClick={() => handleLinkClick('cast')}
+          >
+            Cast
+          </Link>
+          <Link
+            to={`/movies/${movieId}/reviews`}
+            className={`${styles.linkReviews} ${
+              activeSection === 'reviews' ? styles.activeLink : ''
+            }`}
+            onClick={() => handleLinkClick('reviews')}
+          >
+            Reviews
+          </Link>
         </div>
       </div>
-      <Outlet />
+      {activeSection === 'cast' && <Outlet />}
+      {activeSection === 'reviews' && <Outlet />}
     </div>
   );
 };
